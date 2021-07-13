6c3dd9b22857a4c54bccccea43acd47b2b16eb6d
Adam Swiezowski <adam.swiezowski+git@gmail.com>
2021-07-13 20:20:04 +0200

    support for song editing

--- routes/songs.js
+++ routes/songs.js
@@ -1,8 +1,9 @@
 var express = require('express');
 var router = express.Router();
 const fs = require('fs');
+const fsPromises = require('fs').promises;
 var sanitizeFilename = require("sanitize-filename");
-const {SONG_DIR} = require('./constants')
+const { SONG_DIR } = require('./constants')
 const SUPPORTED_FORMATS = ['chordpro', 'ultimateguitar', 'regular'];
 
 router.get('/', function (req, res, next) {
@@ -53,5 +54,70 @@ router.post('/', function (req, res, next) {
     }
 
 });
+
+router.put('/', async (req, res, next) => {
+    const { body } = req;
+    if (!body || !body.name || !body.oldName || !body.format || !body.chords) {
+        res.status(400).json({
+            "error": "name, oldName, format and chords fields are required"
+        });
+    }
+    else if (!SUPPORTED_FORMATS.includes(body.format)) {
+        res.status(400).json({
+            "error": "format field has to have one of value: " + SUPPORTED_FORMATS
+        });
+    }
+    else if (!await fileExistsAndIsWritable(SONG_DIR + sanitizeFilename(body.oldName + "." + body.format))) {
+        res.status(500).json({
+            error: "song doesn't exist or can't be renamed"
+        });
+    }
+    else if (body.name !== body.oldName && await fileExist(SONG_DIR + sanitizeFilename(body.name + "." + body.format))) {
+        res.status(400).json({
+            error: "song already exists"
+        });
+    } else {
+        try {
+            await fsPromises.writeFile(SONG_DIR + sanitizeFilename(body.name + "." + body.format), body.chords);
+            if (body.oldName !== body.name) {
+                try {
+                    await fsPromises.rm(SONG_DIR + sanitizeFilename(body.oldName + "." + body.format));
+                    res.status(200).json();
+                }
+                catch (error) {
+                    res.status(500).json({
+                        "error": "couldn't remove old song"
+                    });
+                    console.log(err);
+                }
+            } else {
+                res.status(200).json();
+            }
+        } catch (error) {
+            res.status(500).json({
+                "error": "couldn't add song"
+            });
+            console.log(err);
+        }
+    }
+});
+
+const fileExistsAndIsWritable = async (filePath) => {
+    try {
+        await fsPromises.access(filePath, fs.constants.F_OK | fs.constants.W_OK);
+        return true;
+    } catch (err) {
+        return false;
+    }
+}
+
+const fileExist = async (filePath) => {
+    try {
+        await fsPromises.access(filePath, fs.constants.F_OK);
+        return true;
+    } catch (err) {
+        return false;
+    }
+}
 
 module.exports = router;
